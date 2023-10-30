cfa6791500e716a2e5dcecd3a6a40f01ec07d41b
rober contreras <trebor.contrerasrivera@gmail.com>
2023-10-30 13:37:04 -0500

    users_crud

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -31,7 +31,7 @@ const update =catchError(async(req,res)=>{
         first_name,last_name,email,password,birthday
     },{where:{id},returning:true}
     )
-    return res.json(user)
+    return res.json(user[1][0])
 })
 
 module.exports = {
